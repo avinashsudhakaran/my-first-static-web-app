5b5444c6666acf53689997ba2b6c834436c61d94
Avinash <avinash.sudhakaran@woodplc.com>
2021-09-21 10:58:25 +0800

    added onlicks for all the buttons

---
+++ src/scripts/dashboardcode.js
@@ -0,0 +1,11 @@
+
+
+function redirectToRequest(){
+    window.open("../requestform?id="+"123"+"&key="+"1800555","_blank");
+}
+
+
+function redirectToReport(){
+    window.open("../reportform?id="+"987"+"&key="+"2000888","_blank");
+
+}
