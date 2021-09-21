3b64c36e258ada1fa10705f27c719fdc0bfd48b6
Avinash <avinash.sudhakaran@woodplc.com>
2021-09-21 11:51:47 +0800

    changes url search params in request form code

--- src/scripts/requestformcode.js
+++ src/scripts/requestformcode.js
@@ -3,9 +3,11 @@ window.onload = (event) => {
     //returns the status of ajax request to calling function
     
     if(/[?&]id=/.test(location.search)){
-        var id = getUrlParameter('id');
-        var requestID = getUrlParameter('key');
-        console.log(id + "      " + requestID);
+        var url_string = window.location.href; //window.location.href
+        var url = new URL(url_string);
+        var id = url.searchParams.get("id");
+        var key = url.searchParams.get("key");
+        console.log(id +  "      " + key);
     }else{
         console.log("query not found");
     }
