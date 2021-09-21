97ff76b19ffb3bd54d3cd036c7448569573580fa
Avinash <avinash.sudhakaran@woodplc.com>
2021-09-21 11:46:36 +0800

    changed jquery source

--- src/scripts/requestformcode.js
+++ src/scripts/requestformcode.js
@@ -1,10 +1,11 @@
 window.onload = (event) => {
     
     //returns the status of ajax request to calling function
-    var id = getUrlParameter('id');
-    var requestID = getUrlParameter('key');
+    
     if(/[?&]id=/.test(location.search)){
-        console.log(id + "      " + key);
+        var id = getUrlParameter('id');
+        var requestID = getUrlParameter('key');
+        console.log(id + "      " + requestID);
     }else{
         console.log("query not found");
     }
