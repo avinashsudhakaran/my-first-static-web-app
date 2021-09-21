678098c9c0395610bb46fb5f8f86a447f0f42de6
Avinash <avinash.sudhakaran@woodplc.com>
2021-09-21 15:29:17 +0800

    working API and can get user details from authenticator

--- api/requestFormAPI/__init__.py
+++ api/requestFormAPI/__init__.py
@@ -6,13 +6,15 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     output = req.get_json()
-    # if(output['id'] == '123' & output['key'] == '1800555'):
-    #     name = "my name jeff"
+    print(output)
+    if(output['id'] == '123' and output['key'] == '1800555'):
+        name = "my name jeff"
+        print(name)
 
-    # if name:
-    return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    # else:
-        # return func.HttpResponse(
-        #      "Failed",
-        #      status_code=200
-        # )
+    if name:
+        return func.HttpResponse(f"Hello. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "Failed",
+             status_code=200
+        )
